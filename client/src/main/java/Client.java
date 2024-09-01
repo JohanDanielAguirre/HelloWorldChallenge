41df0e18d529e7c49d78968b70b49821ab1c4b29
Johan Daniel Aguirre Arias <aguirreariasjohandaniel@gmail.com>
2024-09-01 16:13:37 -0500

    refactor: refactor all the project

--- client/src/main/java/Client.java
+++ client/src/main/java/Client.java
@@ -1,29 +1,25 @@
 import Demo.Response;
 
-import java.util.Scanner;
-
-public class Client {
-    public static void main(String[] args) {
+public class Client
+{
+    public static void main(String[] args)
+    {
         java.util.List<String> extraArgs = new java.util.ArrayList<>();
 
-        try (com.zeroc.Ice.Communicator communicator = com.zeroc.Ice.Util.initialize(args, "config.client", extraArgs)) {
+        try(com.zeroc.Ice.Communicator communicator = com.zeroc.Ice.Util.initialize(args,"config.client",extraArgs))
+        {
             //com.zeroc.Ice.ObjectPrx base = communicator.stringToProxy("SimplePrinter:default -p 10000");
             Response response = null;
             Demo.PrinterPrx service = Demo.PrinterPrx
                     .checkedCast(communicator.propertyToProxy("Printer.Proxy"));
-
-            if (service == null) {
+            
+            if(service == null)
+            {
                 throw new Error("Invalid proxy");
             }
+            response = service.printString("Hello World from a remote client!");
 
-            Scanner sc = new Scanner(System.in);
-            String s = sc.nextLine();
-
-            while (!s.equals("exit")) {
-                response = service.printString(s);
-                System.out.println("Respuesta del server: " + response.value + ", " + response.responseTime);
-                s = sc.nextLine();
-            }
+            System.out.println("Respuesta del server: " + response.value + ", " + response.responseTime);
         }
     }
 }
